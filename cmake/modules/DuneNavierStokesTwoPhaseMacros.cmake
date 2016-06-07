39eaed2fe5b8389f6ddc359b8cbd2cf5a88f145d
Marco Agnese <m.agnese13@imperial.ac.uk>
2016-06-07 13:34:58 +0100

    [buildsystem] mark as required the necessary modules/libraries

--- cmake/modules/DuneNavierStokesTwoPhaseMacros.cmake
+++ cmake/modules/DuneNavierStokesTwoPhaseMacros.cmake
@@ -1,3 +1,8 @@
-# File for module specific CMake tests.
-find_package(GMSH)
+find_package(Alberta REQUIRED)
+include(AddAlbertaFlags)
+
+find_package(GMSH REQUIRED)
 include(AddGMSHFlags)
+
+find_package(SuiteSparse OPTIONAL_COMPONENTS LDL SPQR UMFPACK REQUIRED)
+include(AddSuiteSparseFlags)
